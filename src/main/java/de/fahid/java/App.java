d7a25b8c63d3894fa5f4bacaa4323a28a159f2b9
Fahid Shehzad <git@shehzad.eu>
2022-05-12 01:52:48 +0200

    Gui auf start eingesetzt

--- src/main/java/de/fahid/java/App.java
+++ src/main/java/de/fahid/java/App.java
@@ -1,7 +1,16 @@
 package de.fahid.java;
 
+import de.fahid.java.gui.GuiPcVerwaltung;
+
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Locale.setDefault(Locale.US);
+        //Locale.setDefault(Locale.GERMANY);
+
+        System.out.println(LanguageManager.Language.getString("HelloWorld"));
+        GuiPcVerwaltung gui = new GuiPcVerwaltung();
+        gui.showGui();
     }
 }
