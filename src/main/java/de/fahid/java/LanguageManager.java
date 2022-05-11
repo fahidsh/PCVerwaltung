8a0b6e8ec87fc382f7edc3bdbf1e7464b2db38b1
Fahid Shehzad <git@shehzad.eu>
2022-05-12 01:54:56 +0200

    getString methode addiert

--- src/main/java/de/fahid/java/LanguageManager.java
+++ src/main/java/de/fahid/java/LanguageManager.java
@@ -1,2 +1,19 @@
-package de.fahid.java;public class LanguageManager {
+package de.fahid.java;
+
+import java.util.ResourceBundle;
+
+public class LanguageManager {
+    private static LanguageManager instance;
+    public final static String LanguageResourceBundleString = "language";
+    public static ResourceBundle Language = ResourceBundle.getBundle(LanguageResourceBundleString);
+
+    public static String getString(String key) {
+        String value = null;
+        if(!Language.containsKey(key)) {
+            value = Language.getString("StringNotFound");
+        } else {
+            value = Language.getString(key);
+        }
+        return value;
+    }
 }
