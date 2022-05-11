ecd1a90ee0a82eb96fb849ff2831832ccde680c8
Fahid Shehzad <git@shehzad.eu>
2022-05-12 01:19:44 +0200

    Basic Gui erstellt

---
+++ src/main/java/de/fahid/java/gui/GuiPcVerwaltung.java
@@ -0,0 +1,30 @@
+package de.fahid.java.gui;
+
+import javax.swing.*;
+
+public class GuiPcVerwaltung {
+    private JPanel pnlMain;
+    private JPanel pnlHeader;
+    private JPanel pnlStatus;
+    private JPanel pnlSrollbar;
+    private JPanel pnlNavigation;
+    private JPanel pnlCommandButtons;
+    private JPanel pnlPcProperties;
+    private JPanel PnlPcSummary;
+    private JLabel lblAppName;
+    private JLabel lblAppVersion;
+    private JPanel pnlHeaderSpacer;
+    private JLabel lblStatus;
+    private JScrollBar sbNavigation;
+    private JButton btnFirst;
+    private JButton btnPrevious;
+    private JButton btnNext;
+    private JButton btnLast;
+    private JButton btnAdd;
+    private JButton btnSave;
+    private JButton btnExit;
+    private JTextField txtRam;
+    private JTextField txtCpu;
+    private JTextField txtHdd;
+    private JTextArea taPcSummary;
+}
